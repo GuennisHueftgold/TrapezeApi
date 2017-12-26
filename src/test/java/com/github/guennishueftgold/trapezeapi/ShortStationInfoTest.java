8ae6a436640281909c64636ebfc00630ef870891
GuennisHueftgold <34449444+GuennisHueftgold@users.noreply.github.com>
2017-12-26 11:17:37 +0100

    ShortStationInfo#hashCode tests

--- src/test/java/com/github/guennishueftgold/trapezeapi/ShortStationInfoTest.java
+++ src/test/java/com/github/guennishueftgold/trapezeapi/ShortStationInfoTest.java
@@ -32,18 +32,48 @@ public class ShortStationInfoTest {
 
     @Test
     public void ShortStationInfo_equals_should_be_true() {
-        final ShortStationInfo shortStationInfo1 = new ShortStationInfo.Builder().setId("id1").build();
-        final ShortStationInfo shortStationInfo2 = new ShortStationInfo.Builder().setId("id1").build();
+        final ShortStationInfo shortStationInfo1 = new ShortStationInfo.Builder()
+                .setId("id1")
+                .setStopShortName("shortName")
+                .setStopName("stopName")
+                .build();
+        final ShortStationInfo shortStationInfo2 = new ShortStationInfo.Builder()
+                .setId("id1")
+                .setStopShortName("shortName")
+                .setStopName("stopName")
+                .build();
         assertEquals(shortStationInfo1, shortStationInfo2);
+        assertEquals(shortStationInfo1, shortStationInfo1);
     }
 
     @Test
     public void ShortStationInfo_equals_should_be_false() {
-        final ShortStationInfo shortStationInfo1 = new ShortStationInfo.Builder().setId("id1").build();
-        final ShortStationInfo shortStationInfo2 = new ShortStationInfo.Builder().setId("id2").build();
-        assertNotEquals(shortStationInfo1, shortStationInfo2);
+        final ShortStationInfo shortStationInfo1 = new ShortStationInfo.Builder()
+                .setId("id1")
+                .setStopShortName("shortName")
+                .setStopName("stopName")
+                .build();
         assertNotEquals(shortStationInfo1, null);
         assertNotEquals(shortStationInfo1, new Object());
+        for (int i = 0; i < 3; i++) {
+            final ShortStationInfo.Builder builder = new ShortStationInfo.Builder()
+                    .setId("id1")
+                    .setStopShortName("shortName")
+                    .setStopName("stopName");
+            switch (i) {
+                case 0:
+                    builder.setId("id2");
+                    break;
+                case 1:
+                    builder.setStopName("id2");
+                    break;
+                case 2:
+                    builder.setStopShortName("id2");
+                    break;
+            }
+            final ShortStationInfo shortStationInfo2 = builder.build();
+            assertNotEquals(shortStationInfo1, shortStationInfo2);
+        }
     }
 
     @Test
